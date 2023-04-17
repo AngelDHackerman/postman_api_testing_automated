ddb6273b33f93de5fa556da396e53b2d2bb04ff3
angelDariaux <angeldariaux@gmail.com>
2023-04-17 14:57:38 -0600

    banner grab, para ver la informacion del servicio que esta usando el puerto 22

---
+++ libros_hacking/bannerGrab.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3 
+
+import socket # sockets, interactua con los sockets de la red
+
+s = socket.socket()
+
+s.connect(("127.0.01", 22))  
+
+answer = s.recv(1024) # Se recibe la respuesta del servidor, con un tamaño máximo de 1024 bytes. La respuesta es guardada en la variable answer.
+
+print(answer) 
+
+s.close()                                                                                                                                  
